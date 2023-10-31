ec32ef1d3e9a256463de92de756b5a4e34b3797f
khesyalv <145529469+khesyalv@users.noreply.github.com>
2023-10-31 10:03:03 +0700

    Add files via upload

---
+++ ModifikasiForKelipatan16.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ModifikasiForKelipatan16 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int kelipatan, total = 0, counter = 0; 
+        double rataRata = 0.0;
+        System.out.println("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                total += i;
+                counter++;
+            }
+        }
+        if (counter > 0){
+            rataRata = (double) total / counter;
+        } 
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+    }
+    }
